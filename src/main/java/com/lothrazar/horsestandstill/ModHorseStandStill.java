0bede5067ed24f06bf32c267567280d761dcf4d3
ACGaming <4818419+ACGaming@users.noreply.github.com>
2021-03-21 09:57:36 +0100

    Fix horse dying in water + no damage

--- src/main/java/com/lothrazar/horsestandstill/ModHorseStandStill.java
+++ src/main/java/com/lothrazar/horsestandstill/ModHorseStandStill.java
@@ -7,20 +7,20 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ModHorseStandStill.MODID,certificateFingerprint = "@FINGERPRINT@", updateJSON = "")
-public class ModHorseStandStill {
+@Mod(modid = ModHorseStandStill.MODID, certificateFingerprint = "@FINGERPRINT@", updateJSON = "")
+public class ModHorseStandStill
+{
+    public static final String MODID = "horsestandstill";
+    public static Logger logger;
 
-  public static final String MODID = "horsestandstill";
-  public static Logger logger;
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(new EventHorseStandStill());
+    }
 
-  @EventHandler
-  public void preInit(FMLPreInitializationEvent event) {
-    logger = event.getModLog();
-    MinecraftForge.EVENT_BUS.register(this);
-    MinecraftForge.EVENT_BUS.register(new EventHorseStandStill());
-  }
-
-  @EventHandler
-  public void init(FMLInitializationEvent event) {}
- 
+    @EventHandler
+    public void init(FMLInitializationEvent event) {}
 }
--- src/main/java/com/lothrazar/horsestandstill/UtilWorld.java
+++ src/main/java/com/lothrazar/horsestandstill/UtilWorld.java
@@ -2,14 +2,13 @@ package com.lothrazar.horsestandstill;
 
 import net.minecraft.util.math.BlockPos;
 
-public class UtilWorld {
-
-  public static double distanceBetweenHorizontal(BlockPos start, BlockPos end) {
-    int xDistance = Math.abs(start.getX() - end.getX());
-    int zDistance = Math.abs(start.getZ() - end.getZ());
-    // ye olde pythagoras
-    return Math.sqrt(xDistance * xDistance + zDistance * zDistance);
-  }
- 
- 
+public class UtilWorld
+{
+    public static double distanceBetweenHorizontal(BlockPos start, BlockPos end)
+    {
+        int xDistance = Math.abs(start.getX() - end.getX());
+        int zDistance = Math.abs(start.getZ() - end.getZ());
+        // ye olde pythagoras
+        return Math.sqrt(xDistance * xDistance + zDistance * zDistance);
+    }
 }
